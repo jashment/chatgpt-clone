277eec5bff8253d8f3fc2cfe18732e3397aa7827
Jordan <jordanashment@gmail.com>
2024-01-25 13:57:32 -0700

    Add scss and bootstrap styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
+import "./styles/custom.scss"
 
 const App = () => {
   return (
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './style/style.css'
 
 const Home = () => {
   const [input, setInput] = useState()
@@ -8,14 +9,58 @@ const Home = () => {
   const [jresult, setJResult] = useState()
 
   const handleSubmit = async (e) => {
+    e.prevendDefault()
+    if (!input) {
+      setError('Please enter a value.')
+      setPrompt('')
+      setResult('')
+      setJResult('')
+      return
+    }
 
+    try {
+      const response = await fetch('/api/chatgpt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: input })
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+      } else {
+        throw new Error('An error occured.')
+      }
+    } catch (error) {
+      console.log(error)
+      setResult('')
+      setError('An error occured while submitting the form.')
+    }
   }
 
 
   return (
     <div className='container'>
       <form className='form-horizontal' onSubmit={handleSubmit}>
-
+        <div className='row form-group mt-2'>
+          <div className='col-sm-10'>
+            <div className='form-floating'>
+              <textarea
+                className='form-control custom-input'
+                id='floatingInput'
+                placeholder='Enter a prompt'
+                value={input}
+                onChange={e => setInput(e.target.value)}
+              />
+              <label htmlFor='floatingInput'>Input</label>
+            </div>
+          </div>
+          <div className='col-sm-2'>
+            <button type='submit' className='btn btn-primary custom-button'>Submit</button>
+          </div>
+        </div>
       </form>
       {error && <div className='alert alert-danger mt-3'>{error}</div>}
       {prompt && <div className='alert alert-secondary mt-3'>{prompt}</div>}
