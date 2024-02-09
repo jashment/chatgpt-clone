92978019b74866eaa4f2f3a57ffc2b6b8ea6f685
Jordan <jordanashment@gmail.com>
2024-02-09 13:15:53 -0700

    Delete original form

---
+++ backend/serverSummary.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { Configuration, OpenAIApi } = require('openai')
+
+const app = express()
+
+app.use(express.json())
+
+dotenv.config()
+app.use(cors())
+
+const configuration = new Configuration({
+  organization: 'org-hlVylsQNv3gdpXxHbosquRRW',
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+const runCompletion = async (prompt) => {
+  const response = await openai.createCompletion({
+    model: 'gpt-3.5-turbo-instruct',
+    prompt: prompt,
+    temperature: 1,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 50,
+    echo: true
+  })
+
+  return response
+}
+
+app.post('/api/chatgpt', async (req, res) => {
+
+  try {
+    const { text } = req.body
+    const completion = await runCompletion(text)
+    res.json({ data: completion.data })
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json(error.response.data)
+    } else {
+      console.error('Error with OpenAI API request', error.message)
+      res.status(500).json({
+        error: {
+          message: 'An Error Occured.'
+        }
+      })
+    }
+  }
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server Started at ${PORT}`))
---
+++ frontend/src/screens/PDFSummary.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import './style/style.css'
+
+const Home = () => {
+  const [input, setInput] = useState()
+  const [error, setError] = useState()
+  const [result, setResult] = useState()
+  const [prompt, setPrompt] = useState()
+  const [jresult, setJResult] = useState()
+  const [maxWords, setMaxWords] = useState()
+  const [selectedFile, setSelectedFile] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFileChange = () => {
+
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!input) {
+      setError('Please enter a value.')
+      setPrompt('')
+      setResult('')
+      setJResult('')
+      return
+    }
+
+    try {
+      const response = await fetch('http://localhost:5001/api/chatgpt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS"
+        },
+        body: JSON.stringify({ text: input })
+      })
+      console.log(response)
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+        setPrompt(input)
+        setResult(data.data.choices[0].text.replace(/.*:/, ""))
+        setJResult(JSON.stringify(data.data, null, 2))
+        setInput('')
+        setError('')
+      } else {
+        throw new Error('An error occured.')
+      }
+    } catch (error) {
+      console.log(error)
+      setResult('')
+      setError('An error occured while submitting the form.')
+    }
+  }
+
+
+  return (
+    <div className='container'>
+      <div className='hero d-flex align-items-center justify-content-center text-center flex-column p-3'>
+        <h1 className='display-4'>PDF Book Summarizer</h1>
+        <p className='lead'>Summarize PDF Books for Efficient Reading!</p>
+        <form className='w-100'>
+          <input type='file' accept='.pdf' onChange={handleFileChange}></input>
+          <div className='form-group row'>
+            <div className='col-sm-4 offset-sm-4 mt-3'>
+              <input type='number' min='10' value={maxWords} onChange={(e) => setMaxWords(e.target.value)} className='form-control'></input>
+            </div>
+            <button type='submit' disabled={!selectedFile || isLoading} className='btn btn-primary custom-button mt-1'> Summarize PDF</button>
+          </div>
+        </form>
+      </div>
+      {error && <div className='alert alert-danger mt-3'>{error}</div>}
+      {result && <div className='alert alert-success mt-3'>{result}</div>}
+      {result && (<pre className='alert alert-success mt-3'><code>{jresult}</code></pre>)}
+    </div>
+  )
+}
+
+export default Home
