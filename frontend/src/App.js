46b9c00e74893ebaa258002ea72529d51832c836
Jordan <jordanashment@gmail.com>
2024-02-09 13:10:05 -0700

    Adding PDFSummary file again...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
 import Stream from './screens/Stream'
 import "./styles/custom.scss"
-import PDFSummary from './screens/PDFSummary/PDFSummary'
+import PDFSummary from './screens/PDFSummary'
 
 const App = () => {
   return (
